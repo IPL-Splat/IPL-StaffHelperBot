818b14d5e9aa219d95ff93a2397d1101400adeaa
jpg <derek.bond24@gmail.com>
2020-08-27 08:08:52 -0500

    Add project files.

---
+++ IPL-StaffHelperBot/Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+ * IPL Staff Helper Bot.
+ * Created by .jpg.
+ * Intended for use by IPL staff.
+ */
+
+using System;
+using System.Threading.Tasks;
+
+namespace IPL_StaffHelperBot
+{
+    class Program
+    {
+        static void Main(string[] args) => new Program();
+
+        Program()
+        {
+            Init().GetAwaiter().GetResult();
+        }
+
+        async Task Init()
+        {
+            //Cool stuff goes here.
+        }
+    }
+}
